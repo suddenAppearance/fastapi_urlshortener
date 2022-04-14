964d568f26679991ab823fea7c49c6d0107e87ce
Damir <kuz.damir.01@gmail.com>
2022-04-14 16:58:33 +0300

    init pytest

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,7 +1,6 @@
 import os.path
 
-from fastapi import APIRouter, Request
-from starlette.responses import FileResponse
+from fastapi import APIRouter
 
 from api.api_v1.endpoints import oauth2, users, urls
 
@@ -10,5 +9,3 @@ router = APIRouter()
 router.include_router(oauth2.router, prefix="/oauth2", tags=["oauth2"])
 router.include_router(users.router, prefix="/users", tags=['users'])
 router.include_router(urls.router, prefix="", tags=['urls'])
-
-
