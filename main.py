09201bd75e55c88d8f32854486ba2fb30341a35b
Damir <kuz.damir.01@gmail.com>
2022-04-14 10:53:07 +0300

    final commit

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.api_v1 import api
 
 app = FastAPI()
 
 app.include_router(api.router, prefix="/api/v1")
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/{url_hash}/")
+async def index(url_hash: str):
+    return RedirectResponse(url=f"/api/v1/{url_hash}/")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
